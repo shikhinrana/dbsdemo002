b79eb0e4d4ba83e574077adb49f7d183b369b0ee
shikhinrana <32670919+shikhinrana@users.noreply.github.com>
2017-10-10 12:51:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 echo "<h1>Hello Universe</h1>";
 echo "<p>hello world this is shikhin rana</p>";
 echo "<p>this is a new paragrah</p>";
-echo "<a href="https://www.netflix.com/browse">netflix</a>";
+
